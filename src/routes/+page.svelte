a93d1f794fc2d4dc5888d2a1550c7de8da37b27c
Adam Rackis <adamrackis@hotmail.com>
2022-12-26 19:44:26 -0600

    Work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,1 @@
-<script lang="ts">
-	import {} from 'a';
-</script>
-
 <h1>Root page</h1>
--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -1,1 +1,5 @@
-<h1>List</h1>
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>{$page.data.todos}</h1>
